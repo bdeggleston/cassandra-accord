52ab87b65cac34eaa63ec28402ab868e964c6abf
Blake Eggleston <blake@ultrablake.com>
2022-08-23 16:12:31 -0700

    rework listener notification from maybeExecute

--- accord-core/src/main/java/accord/local/Command.java
+++ accord-core/src/main/java/accord/local/Command.java
@@ -205,12 +205,12 @@ public abstract class Command implements Listener, Consumer<Listener>, TxnOperat
     }
 
     // relies on mutual exclusion for each key
-    public Future<?> commit(Txn txn, Key homeKey, Key progressKey, Timestamp executeAt, Deps deps)
+    public boolean commit(Txn txn, Key homeKey, Key progressKey, Timestamp executeAt, Deps deps)
     {
         if (hasBeen(Committed))
         {
             if (executeAt.equals(executeAt()) && status() != Invalidated)
-                return Write.SUCCESS;
+                return false;
 
             commandStore().agent().onInconsistentTimestamp(this, (status() == Invalidated ? Timestamp.NONE : this.executeAt()), executeAt);
         }
@@ -258,7 +258,14 @@ public abstract class Command implements Listener, Consumer<Listener>, TxnOperat
         boolean isProgressShard = progressKey != null && handles(txnId().epoch, progressKey);
         commandStore().progressLog().commit(txnId(), isProgressShard, isProgressShard && progressKey.equals(homeKey));
 
-        notifyListeners();
+        return true;
+    }
+
+    public Future<?> commitAndBeginExecution(Txn txn, Key homeKey, Key progressKey, Timestamp executeAt, Deps deps)
+    {
+        if (!commit(txn, homeKey, progressKey, executeAt, deps))
+            return Write.SUCCESS;
+
         return maybeExecute(false);
     }
 
@@ -354,23 +361,22 @@ public abstract class Command implements Listener, Consumer<Listener>, TxnOperat
                 {
                     command.removeListener(this);
                 }
-                maybeExecute(true);
+                maybeExecute(false);
                 break;
         }
     }
 
-    protected void postApply(boolean notifyListeners)
+    protected void postApply()
     {
         status(Applied);
-        if (notifyListeners)
-            notifyListeners();
+        notifyListeners();
     }
 
-    protected Future<?> apply(boolean notifyListeners)
+    protected Future<?> apply()
     {
         return writes().apply(commandStore()).flatMap(unused ->
             commandStore().process(this, commandStore -> {
-                postApply(notifyListeners);
+                postApply();
             })
         );
     }
@@ -380,10 +386,13 @@ public abstract class Command implements Listener, Consumer<Listener>, TxnOperat
         return txn().read(this, readKeys);
     }
 
-    private Future<?> maybeExecute(boolean notifyListeners)
+    private Future<?> maybeExecute(boolean notifyListenersOnNoop)
     {
         if (status() != Committed && status() != Executed)
+        {
+            if (notifyListenersOnNoop) notifyListeners();
             return Write.SUCCESS;
+        }
 
         if (isUnableToApply())
         {
@@ -391,6 +400,7 @@ public abstract class Command implements Listener, Consumer<Listener>, TxnOperat
             if (blockedBy != null)
             {
                 commandStore().progressLog().waiting(blockedBy.txnId, blockedBy.someKeys);
+                if (notifyListenersOnNoop) notifyListeners();
                 return Write.SUCCESS;
             }
             assert !isWaitingOnApply();
@@ -403,11 +413,11 @@ public abstract class Command implements Listener, Consumer<Listener>, TxnOperat
                 status(ReadyToExecute);
                 boolean isProgressShard = progressKey() != null && handles(txnId().epoch, progressKey());
                 commandStore().progressLog().readyToExecute(txnId(), isProgressShard, isProgressShard && progressKey().equals(homeKey()));
-                if (notifyListeners)
-                    notifyListeners();
+                notifyListeners();
                 break;
             case Executed:
-                return apply(notifyListeners);
+                if (notifyListenersOnNoop) notifyListeners();
+                return apply();
         }
         return Write.SUCCESS;
     }
