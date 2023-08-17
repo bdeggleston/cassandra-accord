138d475195382393788d809610049f77103bc32f
Benedict Elliott Smith <benedict@apache.org>
2023-08-17 23:17:37 +0100

    imports

--- accord-core/src/main/java/accord/messages/Commit.java
+++ accord-core/src/main/java/accord/messages/Commit.java
@@ -36,7 +36,6 @@ import org.slf4j.LoggerFactory;
 
 import static accord.local.Status.Committed;
 import static accord.local.Status.Known.DefinitionAndRoute;
-import static accord.local.Status.Known.DefinitionOnly;
 
 public class Commit extends TxnRequest<ReadNack>
 {
